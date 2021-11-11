1fb0a734a7b5c92f029567d63edf2485245a5e85
Alexander Cherednichenko <polzovatel@Alex-MacBook-Air.local>
2021-11-11 18:04:39 +0300

    добавление адаптива для width:375px

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+const burgerBtn = document.querySelector('.burger');
+const menu = document.querySelector('.nav__list');
+const body = document.querySelector('body');
+
+burgerBtn.addEventListener('click', () => {
+   burgerBtn.classList.toggle('active');
+   menu.classList.toggle('active');
+   body.classList.toggle('lock');
+});
+
 const upScroll = document.querySelector('.arrow-up');
 
 upScroll.addEventListener('click', backToTop);
-
 function backToTop() {
    if (window.pageYOffset > 0) {
       window.scrollBy(0, -40);
