b74122511ce60a97c674d5465ddf90455424ac0c
Alexander Cherednichenko <polzovatel@Alex-MacBook-Air.local>
2021-11-12 16:54:04 +0300

    исправление багов

--- js/script.js
+++ js/script.js
@@ -8,12 +8,40 @@ burgerBtn.addEventListener('click', () => {
    body.classList.toggle('lock');
 });
 
-const upScroll = document.querySelector('.arrow-up');
+// const upScroll = document.querySelector('.arrow-up');
 
-upScroll.addEventListener('click', backToTop);
-function backToTop() {
-   if (window.pageYOffset > 0) {
-      window.scrollBy(0, -40);
-      setTimeout(backToTop, 0);
+// upScroll.addEventListener('click', backToTop);
+// function backToTop() {
+//    if (window.pageYOffset > 0) {
+//       window.scrollBy(0, -40);
+//       setTimeout(backToTop, 0);
+//    }
+// }
+
+const scrollUp = () => {
+
+   const upBtn = document.querySelector('.arrow-up');
+
+   function trackScroll() {
+      const scrolled = window.pageYOffset;
+
+      if (scrolled > 520) {
+         upBtn.style.display = 'block';
+      } else {
+         upBtn.style.display = 'none';
+      }
    }
-}
+
+   function backToTop() {
+      if (window.pageYOffset > 0) {
+         window.scrollBy(0, -60);
+         setTimeout(backToTop, 0);
+      }
+   }
+
+   window.addEventListener('scroll', trackScroll);
+   upBtn.addEventListener('click', backToTop);
+
+};
+
+scrollUp();
